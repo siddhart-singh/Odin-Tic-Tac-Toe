461f4ec5c50052a503172a75a11d5e6aad05a495
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-08 09:12:01 +0530

    Add Board Generate

--- src/script.js
+++ src/script.js
@@ -1,19 +1,47 @@
 "use strict";
 
+function players(name, mark) {
+  const playerName = name;
+  const playerMoves = [];
+  const marker = mark;
+
+  function setMoves(...move) {
+    playerMoves.push(move);
+  }
+
+  function getMoves() {
+    return playerMoves;
+  }
+
+  function getMarker() {
+    return marker;
+  }
+
+  return { playerName, getMarker, setMoves, getMoves };
+}
+
+const player1 = players("player1", "X");
+const player2 = players("player2", "O");
 
 function gameBoard() {
-    let board = [];
+  let board = [];
 
-    function createGameBoard() {
-        for (let i = 0; i <=2 ; i++) {
-            board.push([]);
-            for (let j = 0 ; j <=2 ; j++) {
-                board[i].push([]);
-            }
-        }
+  function createGameBoard() {
+    for (let i = 0; i <= 2; i++) {
+      board.push([]);
+      for (let j = 0; j <= 2; j++) {
+        board[i].push(" ");
+      }
     }
-    createGameBoard();
-    console.log(board);
+    player1.getMoves().forEach(([row, column]) => {
+      board[row][column] = player1.getMarker();
+    });
+
+    player2.getMoves().forEach(([row, column]) => {
+      board[row][column] = player2.getMarker();
+    });
+  }
+  createGameBoard();
 }
 
 gameBoard();
