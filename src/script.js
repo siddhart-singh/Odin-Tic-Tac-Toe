005bd92607b6ac3833ae19cecef30c10ae2d0510
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-08 07:15:25 +0530

    Add createboard fn

--- src/script.js
+++ src/script.js
@@ -2,5 +2,18 @@
 
 
 function gameBoard() {
-    
+    let board = [];
+
+    function createGameBoard() {
+        for (let i = 0; i <=2 ; i++) {
+            board.push([]);
+            for (let j = 0 ; j <=2 ; j++) {
+                board[i].push([]);
+            }
+        }
+    }
+    createGameBoard();
+    console.log(board);
 }
+
+gameBoard();
