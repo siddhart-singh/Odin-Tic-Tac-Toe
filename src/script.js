789855152568a5b74ca20ad194b44fde6e0cf6fc
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-08 17:26:42 +0530

    Add player board move fn

--- src/script.js
+++ src/script.js
@@ -20,13 +20,17 @@ function players(name, mark) {
   return { playerName, getMarker, setMoves, getMoves };
 }
 
+
+//Creating Players for testing
 const player1 = players("player1", "X");
 const player2 = players("player2", "O");
 
 function gameBoard() {
   let board = [];
+  let activePlayer = player1;
 
   function createGameBoard() {
+    board = [];
     for (let i = 0; i <= 2; i++) {
       board.push([]);
       for (let j = 0; j <= 2; j++) {
@@ -41,7 +45,17 @@ function gameBoard() {
       board[row][column] = player2.getMarker();
     });
   }
-  createGameBoard();
-}
 
-gameBoard();
+  function makeMove(row, column) {
+    console.log(`Make move ${activePlayer.playerName}`);
+    activePlayer.setMoves(row, column);
+    createGameBoard();
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  }
+
+  function getBoard() {
+    return board;
+  }
+
+  return { createGameBoard, makeMove, getBoard };
+}
